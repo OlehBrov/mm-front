7247f098158e8b0ce5174619049000fdd66f6ac8
OlehBrov <o.dudnyk85@gmail.com>
2024-09-24 14:04:30 +0300

    mod ignore

--- src/redux/selectors/selectors.js
+++
@@ -1,5 +0,0 @@
-export const selectCategories = (state) => state.categories;
-export const selectFilter = (state) => state.filter;
-export const selectCart = (state) => state.cart;
-export const selectAuthorization = (state) => state?.authLocal;
-export const selectProducts = (state) => state.products;
